560c9b8f2f7dbd194095f8eee207fa352b1f554e
adib-the-noob <mdadib550@gmail.com>
2023-09-14 02:39:20 +0600

    User Login added

--- core/app/routers/auth.py
+++ core/app/routers/auth.py
@@ -1,10 +1,16 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
+
 from jose import jwt, JWTError
 
 from utils.auth_utils import (
     authenticate_user,
     create_user,
     create_access_token,
+    get_current_user,
 )
 
 from db import db_dependency
@@ -23,4 +29,54 @@ async def register(user: UserRequest, db: db_dependency):
     token = create_access_token(
         data={"sub": user.email, "id": user.id}
     )
-    return token
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+    }
+
+@router.post("/token", response_model=None)
+async def user_login(
+    form_data : Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: db_dependency
+):
+    if form_data.username is None and form_data.email is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    user = authenticate_user(
+        email=form_data.username, 
+        password=form_data.password, 
+        db=db 
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    token = create_access_token(
+        data={"sub": user.email, "id": user.id}
+    )
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "data" : {
+            "id" : user.id,
+            "username" : user.username,
+            "email" : user.email,
+        }
+    }
+
+
+@router.get("/me", response_model=UserResponse)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_user)],
+    db: db_dependency
+  ):
+    return JSONResponse({
+        "data" : {
+            "id" : current_user.id,
+            "username" : current_user.username,
+            "email" : current_user.email,
+        }
+    })
--- core/app/utils/auth_utils.py
+++ core/app/utils/auth_utils.py
@@ -1,4 +1,6 @@
-from fastapi import HTTPException, status
+from typing import Annotated
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
@@ -10,7 +12,7 @@ from db import db_dependency
 from models.user_model import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
@@ -19,19 +21,19 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
+def get_user(email : str, db: db_dependency):
+    user = db.query(User).filter(User.email == email).first()
+    return user
 
 def authenticate_user(
-    username: str | None, email: str | None, password: str, db: db_dependency
-) -> bool:
-    user = db.query(User).filter(User.username == username).first()
-    if not user:
-        user = db.query(User).filter(User.email == email).first()
+    email: str | None, password: str, db: db_dependency
+) -> dict:
+    user = get_user(email, db)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
         return False
     return user
-
 
 def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
     to_encode = data.copy()
@@ -43,7 +45,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
         )
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+        to_encode, settings.SECRET_KEY, algorithm=settings.AlGORITHM
     )
     return encoded_jwt
 
@@ -66,3 +68,27 @@ def create_user(email: str, password: str, full_name: str, db: db_dependency):
         db.commit()
         db.refresh(user)
         return user
+
+
+def get_current_user(
+        token : Annotated[str, Depends(oauth_scheme)],
+        db: db_dependency
+        ):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="User not authenticated",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.AlGORITHM]
+        )
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise credentials_exception
+    return user
