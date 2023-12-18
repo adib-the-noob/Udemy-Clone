f33e31f00bccc260a72634253d9053c2a1753c85
adib-the-noob <mdadib550@gmail.com>
2023-12-18 21:57:47 +0600

    otp verification via redis added

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register/', views.user_registration, name='user_registration'),
     path('login/', views.user_login, name='user_login'),
+    path('verify/', views.user_otp_verification, name='user_otp_verification'),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import authenticate
+import random
+from django.core.cache import cache
 
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
+from . models import User 
 from . serializers import (
     UserRegisterSerializer,
     UserLoginSerializer
@@ -14,8 +16,11 @@ def user_registration(request):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            otp_obj = random.randint(100000, 999999)
+            cache.set(f"otp:{serializer.data['id']}", otp_obj, timeout=180)
+            print(otp_obj)
             return Response({
-                'message': 'Registration successful',
+                'message': 'Registration successful, please Verify with OTP!',
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
@@ -38,3 +43,25 @@ def user_login(request):
         return Response({
             'message': str(e)
         }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+@api_view(['POST'])
+def user_otp_verification(request):
+    try:
+        email = request.data['email']
+        otp = request.data['otp']
+        
+        user_obj = User.objects.get(email=email)
+        
+        if cache.get(f"otp:{user_obj.id}") == int(otp):
+            return Response({
+                'message': 'OTP verified successfully',
+            }, status=status.HTTP_200_OK)
+        return Response({
+            'message': 'OTP verification failed',
+        }, status=status.HTTP_400_BAD_REQUEST)
+    
+    except Exception as e:
+        return Response({
+            'message': str(e)
+        }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
